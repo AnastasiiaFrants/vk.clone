bec50f7bc8f22b15691fe2f65f07727ffccb03f1
Anastasiia Frants <anastasiia.frants@gmail.com>
2021-04-05 18:40:32 +0300

    new feature

--- vk/LoginViewController.swift
+++ vk/LoginViewController.swift
@@ -6,14 +6,28 @@
 //
 
 import UIKit
-
 class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var usernameTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
 
+
+    @IBAction func buttomTupped(_ sender: Any) {
+        if
+            usernameTextfield.text == "admin",
+            passwordTextfield.text == "qwerty" {
+            print("auth success")
+        }
+        else {
+            print("auth deny")
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        userNameLabel.text = "MyUserName"
+        passwordLabel.text = "Qwerty"
     }
-
-
 }
 
